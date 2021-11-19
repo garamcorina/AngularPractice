596c1659761e7fd04db7780e01139513fa2fb66e
garamcorina <corina.garam@lola.tech>
2021-11-19 14:08:58 +0200

    host listener example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'AngularPractice';
+
+  
+  @HostListener('document:click', ['$event'])
+  onClick(event: PointerEvent) {
+    window.alert(
+      `You clicked on point with coordinates: \n X=${event.pageX} and Y=${event.pageY}`
+    );
+  }
 }
